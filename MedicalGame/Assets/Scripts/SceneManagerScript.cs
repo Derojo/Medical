29262ab1c095a17596ead5e66a5712016f52beaa
Goizz <gijstempel@msn.com>
2016-02-15 15:58:35 +0100

    Building wireframe
    Made al main opties conneted

---
+++ MedicalGame/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    //Loading Home Scene
+    public void LoadLevelPage(string changeScene)
+    {
+        EditorSceneManager.LoadScene(changeScene);
+    }
+}
