083b2a218ecdab7dd53a43d65fe9f4646df9bf6c
Goizz <gijstempel@msn.com>
2016-02-22 08:51:59 +0100

    Timer
    Added timer in Question scenes

---
+++ MedicalGame/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+    public float currentTimer;
+    public float startTimer = 10.0f;
+	// Use this for initialization
+	void Awake ()
+    {
+        currentTimer = startTimer;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentTimer -= Time.deltaTime;
+        Debug.Log(currentTimer);
+
+        if(currentTimer <= 0)
+        {
+            Application.LoadLevel("OutOfTimeScene");
+        }
+	}
+
+
+}
