ec6850ab23f490b83113195290f0fc5f22be5ef3
Goizz <gijstempel@msn.com>
2016-03-23 08:57:52 +0100

    Achievementsystem
    implementing and creating achievement system

---
+++ MedicalGame/Assets/Scripts/Achievement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Achievement 
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private string description;
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+
+    private bool unlocked;
+
+    public bool Unlocked
+    {
+        get { return unlocked; }
+        set { unlocked = value; }
+    }
+
+    private int points;
+
+    public int Points
+    {
+        get { return points; }
+        set { points = value; }
+    }
+
+    private int spriteIndex;
+
+    public int SpriteIndex
+    {
+        get { return spriteIndex; }
+        set { spriteIndex = value; }
+    }
+
+    private GameObject achievementRef;
+
+
+    public Achievement(string name, string description, int points, int SpriteIndex, GameObject achievementRef)
+    {
+        this.name = name;
+        this.description = description;
+        this.unlocked = false;
+        this.points = points;
+        this.spriteIndex = spriteIndex;
+        this.achievementRef = achievementRef;
+
+    }
+
+    public bool EarnAchievement()
+    {
+        if(!unlocked)
+        {
+            //achievementRef.GetComponent<Image>().sprite = AchievementManager.Instance.unlockedSprite;
+            unlocked = true;
+            return true;
+        }
+        return false;
+    }//end bool earnachievement
+}
--- MedicalGame/Assets/Scripts/AchievementManager.cs
+++ MedicalGame/Assets/Scripts/AchievementManager.cs
@@ -1,33 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class AchievementManager : MonoBehaviour {
 
     public GameObject achievementPrefab;
+
+    public Sprite[] sprites;
     private float fadeTime = 1.5f;
+    public GameObject visualAchievement;
+    public Dictionary<string, Achievement> achievements = new Dictionary<string, Achievement>();
+
+    public Sprite unlockedSprite;
+
+    private static AchievementManager instance;
+   /* public static AchievementManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<AchievementManager>;
+            }
+        }
+        return AchievementManager.instance; 
+    }*/
 
 	// Use this for initialization
 	void Start ()
     {
-	}
+        //Creates the general achievments
+        CreateAchievement("AchievementHolder", "Mouse", "You Clicked the mouse button", 10, 0);
+        CreateAchievement("AchievementHolder", "Start", "Starten van je eerste spel", 5, 0);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-
-    }
-    //creating achviement popup
-    public void OnClick()
+	void Update ()
     {
-        //Create visual achievement
-        CreateAchievement("General");
+
+         //creating achviement popup
+         if(Input.GetMouseButtonUp(1))
+         {
+            //Create visual achievement
+            EarnAchievement("Mouse");
+         }
+
+    }//end of update
+
+    public void EarnAchievement(string title)
+    {
+        if( achievements[title].EarnAchievement())
+        {
+            GameObject achievement = (GameObject)Instantiate(visualAchievement);
+            SetAchievementInfo("EarnCanvas", achievement, title);
+            StartCoroutine(HideAchievement(achievement));
+        }
     }
+
+
+   
  
     //Creat achievement prefab
-    public void CreateAchievement(string category)
+    public void CreateAchievement(string parent, string title, string description, int points, int spriteIndex)
     {
         GameObject achievement = (GameObject)Instantiate(achievementPrefab);
-        SetAchievementInfo(category, achievement);
-        StartCoroutine(FadeAchievement(achievement));
+        Achievement newAchievement = new Achievement(name, description, points, spriteIndex, achievement);
+        //saving new achievements to dictionairy
+        achievements.Add(title, newAchievement);
+        SetAchievementInfo(parent, achievement, title);
+        //StartCoroutine(FadeAchievement(achievement));
 
     }
     //Destroy achievement afeter 3 seconds
@@ -35,29 +77,31 @@ public class AchievementManager : MonoBehaviour {
     {
         yield return new WaitForSeconds(3);
         Destroy(achievement);
+
     }
 
 
     //Set information about achievement
-    public void SetAchievementInfo(string category, GameObject achievement)
+    public void SetAchievementInfo(string parent, GameObject achievement, string title)
     {
-        achievement.transform.SetParent(GameObject.Find(category).transform);
+        achievement.transform.SetParent(GameObject.Find(parent).transform);
         achievement.transform.localScale = new Vector3(1, 1, 1);
         achievement.transform.localPosition = new Vector3(0,0, 0);
-       
+        // Getting achievement childeren
+        achievement.transform.GetChild(0).GetComponent<Text>().text = title;
+        achievement.transform.GetChild(1).GetComponent<Text>().text = achievements[title].Description;
+        achievement.transform.GetChild(2).GetComponent<Text>().text = achievements[title].Points.ToString();
+        achievement.transform.GetChild(3).GetComponent<Image>().sprite = sprites [achievements[title].SpriteIndex];
     }
 
     //fade in/out
-    private IEnumerator FadeAchievement(GameObject achievement)
+    /*private IEnumerator FadeAchievement(GameObject achievement)
     {
         CanvasGroup canvasGroup = achievement.GetComponent<CanvasGroup>();
         float rate = 1.0f / fadeTime;
 
         int startAlpha = 0;
         int endAlpha = 1;
-
-        
-        
 
         for (int i = 0; i < 2; i++)
         {
@@ -77,6 +121,6 @@ public class AchievementManager : MonoBehaviour {
 
         Destroy(achievement);
         
-    }
+    }*/
         
 }
