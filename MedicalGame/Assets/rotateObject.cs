f7231e0799541754dcca297f17ed54563263280c
Derojo <bakker.rowin@gmail.com>
2016-05-13 09:15:25 +0200

    changed clothchange scene

---
+++ MedicalGame/Assets/rotateObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateObject : MonoBehaviour {
+	
+	private float baseAngle = 0.0f;
+
+	void OnMouseDown(){
+		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+		pos = Input.mousePosition - pos;
+		baseAngle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+		baseAngle += Mathf.Atan2(transform.up.y, 0) *Mathf.Rad2Deg;
+	}
+
+	void OnMouseDrag(){
+		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+		pos = Input.mousePosition - pos;
+		float ang = Mathf.Atan2(pos.y, pos.x) *Mathf.Rad2Deg;
+		transform.localRotation = Quaternion.AngleAxis(ang, Vector3.up);
+	}
+}
