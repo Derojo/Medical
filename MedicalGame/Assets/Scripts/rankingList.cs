d4ce816f389675cba9609b3c374c3197235619ac
Derojo <bakker.rowin@gmail.com>
2016-06-23 08:13:39 +0200

    Added rankinglist script

---
+++ MedicalGame/Assets/Scripts/rankingList.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gamedonia.Backend;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class rankingList : MonoBehaviour {
+
+	
+	// Use this for initialization
+	void Start () {
+		showTopTenPlayers();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	private void showTopTenPlayers() {
+		float delay = 0;
+		// Get the top 10 players of the game
+		GamedoniaUsers.Search("{}", 10, "{profile.wonAttr:-1}", 0, delegate (bool success, IList data) {
+			if (success) {
+				if (data != null) {
+					for(int i = 0; i < data.Count; i++) {
+						Dictionary<string, object> resultUser = (Dictionary<string, object>)data[i];
+						Dictionary<string, object> resultProfile = (Dictionary<string, object>)resultUser["profile"];
+						GameObject row = gameObject.transform.GetChild(i).gameObject;
+						if(i <= 2) {
+							row.transform.GetChild(2).GetComponent<Image>().sprite = PlayerManager.I.GetRankSprite (int.Parse(resultProfile["lvl"].ToString()));
+							row.transform.GetChild(0).GetComponent<Text>().DOText(resultProfile["name"].ToString(), 2f, true, ScrambleMode.All).SetDelay(delay);
+							delay += .5f;
+						} else {
+							row.transform.GetChild(0).GetComponent<Text>().text = resultProfile["name"].ToString();
+						}
+						string scoreAdd = (int.Parse(resultProfile["wonAttr"].ToString()) > 1 ? " punten" : " punt");
+						row.transform.GetChild(1).GetComponent<Text>().text = resultProfile["wonAttr"].ToString()+scoreAdd;
+						// 
+
+					}
+				}
+			}
+			else {
+
+			}
+		});
+	}
+}
